7a2d12b8998ae0e7e15451b7a66642ed869e0dc4
bookgvi <bookgvi@gmail.com>
2020-10-10 12:21:03 +0300

    tunnig HttpSession

--- src/main/java/Servlets/Listener.java
+++ src/main/java/Servlets/Listener.java
@@ -5,13 +5,30 @@ import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRegistration;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 import java.util.Arrays;
 
 @WebListener
-public class Listener implements ServletContextListener {
+public class Listener implements ServletContextListener, HttpSessionListener {
   public void contextInitialized(ServletContextEvent sce) {
     ServletContext sc = sce.getServletContext();
     ServletRegistration sr = sc.addServlet("SessionServlet", SessionServlet.class);
-    Arrays.stream(new String[]{"/session", "/session"}).forEach(sr::addMapping);
+    Arrays.stream(new String[]{"/session", "/session2"}).forEach(sr::addMapping);
+  }
+
+  @Override
+  public void sessionCreated(HttpSessionEvent se) {
+    System.out.println("-- HttpSessionListener#sessionCreated invoked --");
+    HttpSession session = se.getSession();
+    System.out.println("session id: " + session.getId());
+    System.out.println("session duration: " + session.getMaxInactiveInterval());
+    session.setMaxInactiveInterval(10);
+  }
+
+  @Override
+  public void sessionDestroyed(HttpSessionEvent se) {
+    System.out.println("-- HttpSessionListener#sessionDestroyed invoked --");
   }
 }
